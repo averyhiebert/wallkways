28c8bfa10aa3495010bbe51f52ad5122d4f5863a
Avery Hiebert <averyhiebert@gmail.com>
2024-03-02 17:07:05 -0500

    Rotation SLERP works now I think

--- godot-src/greyboxing/player.gd
+++ godot-src/greyboxing/player.gd
@@ -3,16 +3,22 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const ROTATION_TIME_MS = 250
 
 var mouse_sensitivity = 0.002
 
-const ROTATE_COOLDOWN_TIME_MS = 500 # in ms, how long before we can rotate again
+# rotation stuff
+var rotate_from = Basis()
+var rotate_to = Basis()
+var physics_basis = Basis()
+var rotation_percentage = 1.0
 var last_rotated = 0 # Last time we rotated, used for lockout
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
+	physics_basis = transform.basis
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
@@ -27,12 +33,30 @@ func _input(event):
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 
 func rotate_cooldown():
-	return not (Time.get_ticks_msec() - last_rotated) > ROTATE_COOLDOWN_TIME_MS
+	return not (Time.get_ticks_msec() - last_rotated) > ROTATION_TIME_MS
+
+func start_rotation(from,to):
+	rotation_percentage = 0
+	rotate_from = from
+	rotate_to = to
+	physics_basis = rotate_to
+
+func slerp_rotation(delta):
+	# Note: control directions will be a little off while rotating.
+	if rotation_percentage < 1.0:
+		transform.basis = rotate_from.slerp(rotate_to,rotation_percentage)
+		rotation_percentage += (delta*1000) / ROTATION_TIME_MS
+	else:
+		# Update physics basis to match any camera rotations etc.
+		physics_basis = transform.basis
 
 func _physics_process(delta):
-	var local_velocity = transform.basis.inverse() * velocity
+	#var local_velocity = transform.basis.inverse() * velocity
+	var local_velocity = physics_basis.inverse() * velocity
 	
-	# Add the gravity.
+	slerp_rotation(delta)
+	
+	# Add the gravity and/or start new rotation
 	if is_on_floor():
 		var normal = get_floor_normal()
 		var angle = normal.angle_to(transform.basis[1])
@@ -42,7 +66,8 @@ func _physics_process(delta):
 			var new_transform = transform.rotated(axis,-angle)
 			
 			# TODO Interpolate w/ quaternions
-			transform.basis = new_transform.basis
+			#transform.basis = new_transform.basis
+			start_rotation(transform.basis,new_transform.basis)
 			
 			up_direction = get_floor_normal() # For correct floor detection
 			last_rotated = Time.get_ticks_msec()
@@ -64,6 +89,7 @@ func _physics_process(delta):
 		local_velocity.x = move_toward(velocity.x, 0, SPEED)
 		local_velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-	velocity = transform.basis * local_velocity
+	#velocity = transform.basis * local_velocity
+	velocity = physics_basis * local_velocity
 	
 	move_and_slide()
