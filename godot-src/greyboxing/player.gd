e627b2314b1005cafb48441afff24a1d28e9bbba
Avery Hiebert <averyhiebert@gmail.com>
2024-03-02 20:41:47 -0500

    Fix physics bug

--- godot-src/greyboxing/player.gd
+++ godot-src/greyboxing/player.gd
@@ -2,7 +2,10 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+#const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 10
+const GRAVITY_MULTIPLIER = 2
+const FALL_GRAVITY_MULTIPLIER = 3
 const ROTATION_TIME_MS = 250
 
 var mouse_sensitivity = 0.002
@@ -33,9 +36,10 @@ func _input(event):
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 
 func rotate_cooldown():
-	return not (Time.get_ticks_msec() - last_rotated) > 1.1*ROTATION_TIME_MS
+	return not (Time.get_ticks_msec() - last_rotated) > ROTATION_TIME_MS
 
 func start_rotation(from,to):
+	print("Started rotation")
 	rotation_percentage = 0
 	rotate_from = from
 	rotate_to = to
@@ -44,13 +48,19 @@ func start_rotation(from,to):
 func slerp_rotation(delta):
 	# Note: control directions will be a little off while rotating.
 	if rotation_percentage < 1.0:
+		rotation_percentage += (delta*1000.0) / ROTATION_TIME_MS
+		rotation_percentage = clamp(rotation_percentage,0.0,1.0)
 		transform.basis = rotate_from.slerp(rotate_to,rotation_percentage)
-		rotation_percentage += (delta*1000) / ROTATION_TIME_MS
+		#print((delta*1000.0) / ROTATION_TIME_MS)
 	else:
 		if physics_basis != transform.basis:
 			# Update physics basis to match any camera rotations etc.
 			physics_basis = transform.basis.orthonormalized()
-			print("Updating physics basis")
+
+func avoid_wall_slipping(local_velocity):
+	# If this retursn true, we disable x/z player movement to avoid weird behaviour.
+	#  Kinda janky, will have to find a better way later.
+	return is_on_wall() and not is_on_floor() and abs(local_velocity.y) < 5
 
 func _physics_process(delta):
 	#var local_velocity = transform.basis.inverse() * velocity
@@ -60,10 +70,12 @@ func _physics_process(delta):
 	
 	# Add the gravity and/or start new rotation
 	if is_on_floor():
+		print("Is on floor")
 		var normal = get_floor_normal()
 		var angle = normal.angle_to(transform.basis[1])
+		print("Is on floor")
+		print(angle)
 		if (abs(angle - deg_to_rad(45)) < 0.00001) and not rotate_cooldown():
-			print("rotating")
 			var axis = get_floor_normal().cross(transform.basis[1]).normalized()
 			var new_transform = transform.rotated(axis,-angle)
 			
@@ -74,8 +86,11 @@ func _physics_process(delta):
 			up_direction = get_floor_normal() # For correct floor detection
 			last_rotated = Time.get_ticks_msec()
 			local_velocity.y = 0
+	elif local_velocity.y > 0:
+		local_velocity.y -= GRAVITY_MULTIPLIER * gravity * delta
 	else:
-		local_velocity.y -= gravity * delta
+		# Fall slightly faster after the peak of the jump (less floaty)
+		local_velocity.y -= FALL_GRAVITY_MULTIPLIER*gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -84,7 +99,8 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left","right","forward","back")
 	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
-	if direction:
+	#if direction and not (is_on_wall() and not is_on_floor()):
+	if direction and not avoid_wall_slipping(local_velocity):
 		local_velocity.x = direction.x * SPEED
 		local_velocity.z = direction.z * SPEED
 	else:
